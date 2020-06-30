5ef2a095d2b05a293a31d8b9f5b357af50031b63
zayzay500 <lindsay.pierce@yale.edu>
2020-06-30 16:40:21 -0400

    all activerecord associations functioning

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,8 +2,10 @@ class Resource < ApplicationRecord
 	belongs_to :author
 	has_many :comments
 	has_many :upvotes
-	has_many :users, through: :comments, as: :commenters
-	has_many :users, through: :upvotes, as: :upvoters
+	has_many :commenters, through: :comments, source: :user
+	has_many :upvoters, through: :upvotes, source: :user
+	
+	# has_many :upvoted_resources, through: :upvotes, source: :resource
 	has_many :resources_topics
 	has_many :topics, through: :resources_topics
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	has_many :comments
 	has_many :upvotes
-	has_many :resources, through: :comments, as: :commented_resources
-	has_many :resources, through: :upvotes, as: :upvoted_resources
+	has_many :commented_resources, through: :comments, source: :resource
+	has_many :upvoted_resources, through: :upvotes, source: :resource
 end
