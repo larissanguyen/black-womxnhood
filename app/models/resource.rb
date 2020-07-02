f1248f729d1e6fa38bdcc6ccc3117300afefe939
zayzay500 <lindsay.pierce@yale.edu>
2020-07-02 09:54:33 -0400

    added interaction count for resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -8,4 +8,12 @@ class Resource < ApplicationRecord
 	# has_many :upvoted_resources, through: :upvotes, source: :resource
 	has_many :resources_topics
 	has_many :topics, through: :resources_topics
+	
+	def interaction_count
+		interactions.count
+	end
+	
+	def interactions
+		comments + upvotes
+	end
 end
