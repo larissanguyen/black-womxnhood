c00159b1265f524c300efb8ebb8252f9fdac3087
zayzay500 <lindsay.pierce@yale.edu>
2020-07-03 11:21:56 -0400

    login functioning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     skip_before_action :authenticated, only: [:new, :create]
     
     def new
-    
+        @message = flash[:message]
     end
 
     def create
@@ -11,11 +11,23 @@ class SessionsController < ApplicationController
 
         @user = User.find_by(name: params[:name])
         # byebug
-        if @user && @user.authenticate(params[:password])
+        if valid_login?
+            # byebug
             session[:user_id] = @user.id
             redirect_to user_path(@user)
+        elsif !user_exist?
+            flash[:message] = "No user found with that name"
+            # byebug
+            redirect_to "/login"
+        elsif !correct_password?
+            
+            flash[:message] = "Incorrect password"
+            # byebug
+            redirect_to "/login"
         else
-            flash[:errors] = @user.errors.full_messages
+           
+            flash[:message] = "Sorry, unknown error"
+            # byebug
             redirect_to "/login"
         end
     end
@@ -23,5 +35,19 @@ class SessionsController < ApplicationController
     def destroy
         session.delete(:user_id)
         redirect_to "/login"
+    end
+    
+    private
+    
+    def user_exist?
+        @user != nil
+    end
+    
+    def correct_password?
+        @user.authenticate(params[:password])
+    end
+    
+    def valid_login?
+        user_exist? && correct_password?
     end
 end
